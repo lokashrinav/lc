d6a27df8db8ab82f86cd4a7f7ec92ca83e36f132
Shrinav Loka <lokashrinav@gmail.com>
2024-12-31 00:39:53 +0000

    Code challenge submission for question: 633 Sum of Square Numbers

---
+++ 633 Sum of Square Numbers/633sum-of-square-numbers.py
@@ -0,0 +1,15 @@
+class Solution:
+    def judgeSquareSum(self, c: int) -> bool:
+        a = 0
+        b = int(c**0.5) 
+        
+        while a <= b:
+            current_sum = a * a + b * b
+            if current_sum == c:
+                return True
+            elif current_sum < c:
+                a += 1
+            else:
+                b -= 1
+        
+        return False
