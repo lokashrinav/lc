c76c4a37f7644e34618045998cfd7856311b4d6a
Shrinav Loka <lokashrinav@gmail.com>
2025-01-08 09:40:00 +0000

    Code challenge submission for question: 2849 Determine if a Cell Is Reachable at a Given Time

---
+++ 2849 Determine if a Cell Is Reachable at a Given Time/2849determine-if-a-cell-is-reachable-at-a-given-time.py
@@ -0,0 +1,6 @@
+class Solution:
+    def isReachableAtTime(self, sx: int, sy: int, fx: int, fy: int, t: int) -> bool:
+        if sx == fx and sy == fy:
+            return t != 1
+        calc = max(abs(fx - sx), abs(fy - sy))
+        return calc <= t
