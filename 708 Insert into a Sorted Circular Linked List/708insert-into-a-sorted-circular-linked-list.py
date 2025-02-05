13bc00e8009adc3c93dd2afc76521648192d20cc
Shrinav Loka <lokashrinav@gmail.com>
2025-02-05 17:49:41 +0000

    Code challenge submission for question: 708 Insert into a Sorted Circular Linked List

---
+++ 708 Insert into a Sorted Circular Linked List/708insert-into-a-sorted-circular-linked-list.py
@@ -0,0 +1,28 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val=None, next=None):
+        self.val = val
+        self.next = next
+"""
+
+class Solution:
+    def insert(self, head: 'Optional[Node]', insertVal: int) -> 'Node':
+        new_node = Node(insertVal)
+        if head is None:
+            new_node.next = new_node
+            return new_node
+
+        cur = head
+        while True:
+            if cur.val <= insertVal <= cur.next.val:
+                break
+            if cur.val > cur.next.val and (insertVal >= cur.val or insertVal <= cur.next.val):
+                break
+            cur = cur.next
+            if cur == head:
+                break
+
+        new_node.next = cur.next
+        cur.next = new_node
+        return head
