a3095ea5125252fedd91038e5898d6f6f808f157
Shrinav Loka <lokashrinav@gmail.com>
2025-04-24 12:17:40 +0000

    Code challenge submission for question: 189 Rotate Array

--- 189 Rotate Array/189rotate-array.py
+++ 189 Rotate Array/189rotate-array.py
@@ -1,4 +1,34 @@
 class Solution:
     def rotate(self, nums: List[int], k: int) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+
+
         k = k % len(nums)
-        nums[:] = nums[-k:] + nums[:-k]
+
+        if not k:
+            return nums
+            
+        for i in range(len(nums)):
+            ind = i
+
+            if type(nums[ind]) is int:
+                saved = nums[len(nums) + (ind - k)]
+
+            while type(nums[ind]) is int:
+                idk = nums[ind]
+                nums[ind] = (saved, None)
+                saved = idk
+                ind = (ind + k) % len(nums)
+        
+        for i in range(len(nums)):
+            nums[i] = nums[i][0]
+
+        return nums
+                    
+
+
+
+        
+        
