f446093d86af9c48543ce450f4ad00c8d790598f
Shrinav Loka <lokashrinav@gmail.com>
2025-04-29 05:51:36 +0000

    Code challenge submission for question: 311 Sparse Matrix Multiplication

--- 311 Sparse Matrix Multiplication/311sparse-matrix-multiplication.py
+++ 311 Sparse Matrix Multiplication/311sparse-matrix-multiplication.py
@@ -1,39 +1,38 @@
+from collections import defaultdict
+from typing import List
+
 class Solution:
     def multiply(self, mat1: List[List[int]], mat2: List[List[int]]) -> List[List[int]]:
-        m = len(mat1)
-        n = len(mat1[0])
-        k = len(mat2[0])
-
+        m, n, k = len(mat1), len(mat1[0]), len(mat2[0])
+        
+        # Store nonzero elements of mat1
         hmap = defaultdict(list)
         for i in range(m):
-            for p in range(n):
-                if mat1[i][p] != 0:
-                    hmap[i].append(p)
+            for j in range(n):
+                if mat1[i][j] != 0:
+                    hmap[i].append((j, mat1[i][j]))  # Store index and value
         
+        # Store nonzero elements of transposed mat2
         hmap2 = defaultdict(list)
-        for i in range(n):
+        for j in range(n):
             for p in range(k):
-                if mat2[i][p] != 0:
-                    hmap2[p].append(i)
-
-        new_mat = [[0 for i in range(k)] for p in range(m)]
-
-        def dop(arr1, arr2, i, p):
-            total = 0
-            print(i, p, hmap, hmap2)
-            for key in hmap[i]:
-                if key in hmap2[p]:
-                    total += arr1[key] * arr2[key]
-                
-            return total
-                
+                if mat2[j][p] != 0:
+                    hmap2[p].append((j, mat2[j][p]))  # Store index and value
+        
+        # Result matrix
+        new_mat = [[0] * k for _ in range(m)]
+        
+        # Compute product
         for i in range(m):
             for p in range(k):
-                new_mat[i][p] = dop(mat1[i], [mat2[z][p] for z in range(n)], i, p)
+                sum_val = 0
+                nonzero_1 = hmap[i]
+                nonzero_2 = {j: val for j, val in hmap2[p]}  # Convert list to dictionary for O(1) lookup
+                
+                for j, v1 in nonzero_1:
+                    if j in nonzero_2:
+                        sum_val += v1 * nonzero_2[j]
+
+                new_mat[i][p] = sum_val
         
         return new_mat
-
-
-
-
-
