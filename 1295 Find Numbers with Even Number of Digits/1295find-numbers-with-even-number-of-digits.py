af529151092d06b96a98b7151fd0a4dd49fd4688
Shrinav Loka <lokashrinav@gmail.com>
2025-04-30 01:11:35 +0000

    Code challenge submission for question: 1295 Find Numbers with Even Number of Digits

---
+++ 1295 Find Numbers with Even Number of Digits/1295find-numbers-with-even-number-of-digits.py
@@ -0,0 +1,9 @@
+class Solution:
+    def findNumbers(self, nums: List[int]) -> int:
+        
+        total = 0
+        for num in nums:
+            if len(str(num)) % 2 == 0:
+                total += 1
+        
+        return total
