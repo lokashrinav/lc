429f73d59d550f84e8254829ddb3e18ddf2ae162
Shrinav Loka <lokashrinav@gmail.com>
2025-05-04 07:01:01 +0000

    Code challenge submission for question: 2654 Minimum Number of Operations to Make All Array Elements Equal to 1

--- 2654 Minimum Number of Operations to Make All Array Elements Equal to 1/2654minimum-number-of-operations-to-make-all-array-elements-equal-to-1.py
+++ 2654 Minimum Number of Operations to Make All Array Elements Equal to 1/2654minimum-number-of-operations-to-make-all-array-elements-equal-to-1.py
@@ -5,16 +5,7 @@ class Solution:
         if 1 in nums:
             return n - nums.count(1)
         
-        min_subarray_length = float('inf')
-        for i in range(n):
-            current_gcd = nums[i]
-            for j in range(i, n):
-                current_gcd = gcd(current_gcd, nums[j])
-                if current_gcd == 1:
-                    min_subarray_length = min(min_subarray_length, j - i + 1)
-                    break
-        
-        if min_subarray_length == float('inf'):
+        if reduce(gcd, nums) > 1:
             return -1
         
-        return (min_subarray_length - 1) + (n - 1)
+        return n
