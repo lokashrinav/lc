dd2d1aa82aa886f25a9243d7aa591d4e314bacd9
Shrinav Loka <lokashrinav@gmail.com>
2025-05-17 04:37:06 +0000

    Code challenge submission for question: 1822 Sign of the Product of an Array

---
+++ 1822 Sign of the Product of an Array/1822sign-of-the-product-of-an-array.py
@@ -0,0 +1,14 @@
+class Solution:
+    def arraySign(self, nums: List[int]) -> int:
+
+        if 0 in nums:
+            return 0
+
+        neg = 0
+        for i in range(len(nums)):
+            if nums[i] < 0:
+                neg += 1
+        
+        return 1 if neg % 2 == 0 else -1
+                
+        
