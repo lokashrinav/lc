6fd5622681619829fcd4dd4ad5486c57273cca62
Shrinav Loka <lokashrinav@gmail.com>
2025-05-31 06:47:34 +0000

    Code challenge submission for question: 2540 Minimum Common Value

---
+++ 2540 Minimum Common Value/2540minimum-common-value.py
@@ -0,0 +1,22 @@
+class Solution:
+    def getCommon(self, nums1: List[int], nums2: List[int]) -> int:
+
+        l, r = 0, 0 
+
+        final = -1
+
+        while l < len(nums1) and r < len(nums2):
+            if nums1[l] == nums2[r]:
+                return nums1[l]
+            
+            if nums1[l] > nums2[r]:
+                r += 1
+            else:
+                l += 1
+        
+        return -1
+
+
+
+
+        
