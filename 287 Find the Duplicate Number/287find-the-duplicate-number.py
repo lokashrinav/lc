a7fb59d792d7eeb32d7007b0171f6c556cb942d0
Shrinav Loka <lokashrinav@gmail.com>
2025-05-31 20:57:59 +0000

    Code challenge submission for question: 287 Find the Duplicate Number

--- 287 Find the Duplicate Number/287find-the-duplicate-number.py
+++ 287 Find the Duplicate Number/287find-the-duplicate-number.py
@@ -1,23 +1,13 @@
 class Solution:
     def findDuplicate(self, nums: List[int]) -> int:
-        turtle = nums[0]
-        hare = nums[0]
 
-        while True:
-            turtle = nums[turtle]
-            hare = nums[nums[hare]]
-            if turtle == hare:
-                break
+        for i in range(len(nums)):
+            if nums[abs(nums[i]) - 1] < 0:
+                return abs(nums[i])
+            nums[abs(nums[i]) - 1] *= -1
         
-        turtle = nums[0]
-        while turtle != hare:
-            turtle = nums[turtle]
-            hare = nums[hare]
-
-        return turtle
-
-
-        return hare
-
-
-
+        for i in range(len(nums) - 1):
+            if nums[i] > 0:
+                return i + 1
+            
+        
