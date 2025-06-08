f307ad6ad5b827738726092ef159851072bd4d85
Shrinav Loka <lokashrinav@gmail.com>
2025-06-08 07:00:06 +0000

    Code challenge submission for question: 509 Fibonacci Number

--- 509 Fibonacci Number/509fibonacci-number.py
+++ 509 Fibonacci Number/509fibonacci-number.py
@@ -1,11 +1,24 @@
 class Solution:
     def fib(self, n: int) -> int:
-        if n <= 1:
-            return n
-        p1, p2 = 0, 1
+        if n == 0:
+            return 0
 
-        for i in range(2, n + 1):
-            curr = p1 + p2
-            p1 = p2
-            p2 = curr
-        return p2
+        def multiply(A, B):
+            return [
+                [A[0][0] * B[0][0] + A[0][1] * B[1][0], A[0][0] * B[0][1] + A[0][1] * B[1][1]],
+                [A[1][0] * B[0][0] + A[1][1] * B[1][0], A[1][0] * B[0][1] + A[1][1] * B[1][1]]
+            ]
+
+        def matrix_pow(mat, power):
+            # Identity matrix
+            result = [[1, 0], [0, 1]]
+            while power > 0:
+                if power % 2 == 1:
+                    result = multiply(result, mat)
+                mat = multiply(mat, mat)
+                power //= 2
+            return result
+
+        base = [[1, 1], [1, 0]]
+        res = matrix_pow(base, n - 1)
+        return res[0][0]
