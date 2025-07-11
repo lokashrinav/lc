41ed931404d4c8339bc05bc165cd22ce145358e8
Shrinav Loka <lokashrinav@gmail.com>
2025-07-11 05:49:22 +0000

    Code challenge submission for question: 616 Add Bold Tag in String

---
+++ 616 Add Bold Tag in String/616add-bold-tag-in-string.py
@@ -0,0 +1,29 @@
+class Solution:
+    def addBoldTag(self, s: str, words: List[str]) -> str:
+        s2 = s
+        s = list(s)
+        curr = [0] * len(s)
+
+        for i in range(len(s)):
+            flag = 0
+            for p in range(len(words)):
+                #print(s[i:i+len(words[p])], words[p])
+                if s2[i:i+len(words[p])] == words[p]:
+                    flag = max(flag, len(words[p]))
+
+            if flag:
+                for p in range(i, i + flag):
+                    curr[p] = 1
+        
+                
+        res = []
+        for i in range(len(s)):
+            if curr[i] == 1 and (i == 0 or curr[i - 1] == 0):
+                res.append("<b>")
+            res.append(s[i])
+            if curr[i] == 1 and (i == len(s) - 1 or curr[i + 1] == 0):
+                res.append("</b>")
+        
+        return ''.join(res)
+
+        
