6759b1d5197162d5978be0c04af7f000cc2435d1
Shrinav Loka <lokashrinav@gmail.com>
2025-07-15 22:57:51 +0000

    Code challenge submission for question: 2661 First Completely Painted Row or Column

--- 2661 First Completely Painted Row or Column/2661first-completely-painted-row-or-column.py
+++ 2661 First Completely Painted Row or Column/2661first-completely-painted-row-or-column.py
@@ -1,24 +1,19 @@
 class Solution:
     def firstCompleteIndex(self, arr: List[int], mat: List[List[int]]) -> int:
 
+        rows = defaultdict(int)
+        cols = defaultdict(int)
         hmap = {}
 
-        for i in range(len(mat)):
-            for p in range(len(mat[0])):
-                hmap[mat[i][p]] = (i, p)
-
-        row_count = [0] * len(mat)
-        col_count = [0] * len(mat[0])
+        for y in range(len(mat)):
+            for x in range(len(mat[0])):
+                hmap[mat[y][x]] = (y, x)
 
         for i in range(len(arr)):
             y, x = hmap[arr[i]]
-            row_count[y] += 1
-            col_count[x] += 1
-
-            if row_count[y] == len(mat[0]) or col_count[x] == len(mat):
+            rows[y] += 1
+            cols[x] += 1
+            if rows[y] == len(mat[0]) or cols[x] == len(mat):
                 return i
         
-
-
-
         
