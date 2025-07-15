cafa382edcc46eb2ccd98596b6adc77f6a696362
Shrinav Loka <lokashrinav@gmail.com>
2025-07-15 21:16:56 +0000

    Code challenge submission for question: 71 Simplify Path

--- 71 Simplify Path/71simplify-path.py
+++ 71 Simplify Path/71simplify-path.py
@@ -1,30 +1,22 @@
 class Solution:
     def simplifyPath(self, path: str) -> str:
-        i = 0
-        stack = []
-        while i < len(path):
-            while i < len(path) and path[i] == '/':
-                i += 1
-            
-            str1 = ""
-            while i < len(path) and path[i] != '/':
-                str1 += path[i]
-                i += 1
 
-            if str1 == ".":
-                continue
-            elif str1 == "..":
-                if stack:
-                    stack.pop()
-            else:
-                if str1:
-                    stack.append(str1)
-            
-            i += 1
-        return '/' + '/'.join(stack)
+        elems = path.split("/")
 
-                
-            
+        res = []
+        for elem in elems:
+            if elem:
+                if elem == "..":
+                    if len(res) >= 1:
+                        res.pop()
+                elif elem == ".":
+                    continue
+                else:
+                    res.append(elem)
+        
+        str1 = "/" + '/'.join(res)
+
+        return str1
 
 
         
