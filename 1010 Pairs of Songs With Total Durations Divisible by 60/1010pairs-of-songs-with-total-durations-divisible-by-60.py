5dea518f1dc75fbfa9f237014a0b41c7c932aa5c
Shrinav Loka <lokashrinav@gmail.com>
2025-07-15 22:18:17 +0000

    Code challenge submission for question: 1010 Pairs of Songs With Total Durations Divisible by 60

--- 1010 Pairs of Songs With Total Durations Divisible by 60/1010pairs-of-songs-with-total-durations-divisible-by-60.py
+++ 1010 Pairs of Songs With Total Durations Divisible by 60/1010pairs-of-songs-with-total-durations-divisible-by-60.py
@@ -1,14 +1,15 @@
 class Solution:
     def numPairsDivisibleBy60(self, time: List[int]) -> int:
-
+        
         hmap = defaultdict(int)
         total = 0
-        hmap[60] = 1
+        
         for i in range(len(time)):
-            curr = time[i] % 60
-            if curr in hmap:
-                total += hmap[curr]
-            hmap[(60 - curr) % 60] += 1
+            if time[i] % 60 == 0:
+                total += hmap[0]
+            else:
+                total += hmap[60 - time[i] % 60]
+            hmap[time[i] % 60] += 1
         
         return total
 
