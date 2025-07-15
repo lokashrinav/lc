895fd2bc06c7ef8dff086ed3a74e074122f722c7
Shrinav Loka <lokashrinav@gmail.com>
2025-07-15 21:59:50 +0000

    Code challenge submission for question: 5 Longest Palindromic Substring

--- 5 Longest Palindromic Substring/5longest-palindromic-substring.py
+++ 5 Longest Palindromic Substring/5longest-palindromic-substring.py
@@ -1,33 +1,48 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
-        longest = ""
-        for i in range(len(s)):
-            start, end = i, i
-            curr = ""
-            while start >= 0 and end < len(s) and s[start] == s[end]:
-                if start == end:
-                    curr += s[start]
-                else:
-                    curr = s[start] + curr + s[end]
-                if end - start + 1 > len(longest):
-                    longest = curr
-                start -= 1
-                end += 1
+
+        longest = 1
+        saved = s[0]
 
         for i in range(len(s)):
-            start, end = i, i + 1
-            curr = ""
-            while start >= 0 and end < len(s) and s[start] == s[end]:
-                if start == end:
-                    curr += s[start]
+            l, r = i, i
+            curr = 0
+            queue = deque()
+            while l >= 0 and r < len(s):
+                if s[l] == s[r]:
+                    if l == r:
+                        queue.append(s[l])
+                        curr += 1
+                    else:
+                        queue.appendleft(s[l])
+                        queue.append(s[r])
+                        curr += 2
+                    l -= 1
+                    r += 1
                 else:
-                    curr = s[start] + curr + s[end]
-                if end - start + 1 > len(longest):
-                    longest = curr
-                start -= 1
-                end += 1
-        return longest
+                    break
 
+            if curr > longest:
+                longest = max(longest, curr)
+                saved = ''.join(queue)
 
+            l, r = i - 1, i
+            curr = 0
+            queue = deque()
+            while l >= 0 and r < len(s):
+                if s[l] == s[r]:
+                    curr += 2
+                    queue.appendleft(s[l])
+                    queue.append(s[r])
+                    l -= 1
+                    r += 1
+                else:
+                    break
+
+            if curr > longest:
+                longest = max(longest, curr)
+                saved = ''.join(queue) 
         
-        
+        return saved
+            
+
