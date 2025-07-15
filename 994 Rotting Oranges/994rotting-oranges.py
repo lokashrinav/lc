4c85126b75730cb4312c8dd2123c579e53a31068
Shrinav Loka <lokashrinav@gmail.com>
2025-07-15 17:58:40 +0000

    Code challenge submission for question: 994 Rotting Oranges

--- 994 Rotting Oranges/994rotting-oranges.py
+++ 994 Rotting Oranges/994rotting-oranges.py
@@ -1,37 +1,41 @@
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
-        length = len(grid[0])
-        width = len(grid)
-        visited = set()
-        queue = deque()
-        fresh_oranges = 0
-        for y in range(width):
-            for x in range(length):
-                if grid[y][x] == 2:
-                    queue.append((x, y))
-                if grid[y][x] == 1:
-                    fresh_oranges += 1
-        
-        if not queue and fresh_oranges > 0:
-            return -1
 
-        dist = 0
+        queue = deque()
+        total = 0
+
+        for i in range(len(grid)):
+            for p in range(len(grid[0])):
+                if grid[i][p] == 2:
+                    queue.append((i, p))
+                elif grid[i][p] == 1:
+                    total += 1
+
+        if total == 0:
+            return 0
+
+        if not queue:
+            return -1
+        
+        levels = 0
+        dirs = [[1, 0], [0, 1], [-1, 0], [0, -1]]
 
         while queue:
             for i in range(len(queue)):
-                x, y = queue.popleft()
-                if (x, y) in visited or not (x >= 0 and x < length and y >= 0 and y < width) or grid[y][x] == 0:
-                    continue
-                if grid[y][x] == 1:
-                    fresh_oranges -= 1
-                visited.add((x, y))
-                queue.append((x + 1, y))
-                queue.append((x - 1, y))
-                queue.append((x, y + 1))
-                queue.append((x, y - 1))
-                if fresh_oranges == 0:
-                    return dist
-            dist += 1
-        if fresh_oranges > 0:
-            return -1
-        return dist
+                ny, nx = queue.popleft()
+                for y, x in dirs:
+                    dy, dx = ny + y, nx + x
+                    if len(grid) > dy >= 0 and len(grid[0]) > dx >= 0 and grid[dy][dx] == 1:
+                        grid[dy][dx] = 2
+                        total -= 1
+                        queue.append((dy, dx))
+                        if total == 0:
+                            return levels + 1
+            levels += 1
+
+        for i in range(len(grid)):
+            for p in range(len(grid[0])):
+                if grid[i][p] == 1:
+                    return -1
+        
+        return levels
