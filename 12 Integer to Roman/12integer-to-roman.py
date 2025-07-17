1df555a17489756f66b4e22209e1d5fcea33e18c
Shrinav Loka <lokashrinav@gmail.com>
2025-07-17 22:35:23 +0000

    Code challenge submission for question: 12 Integer to Roman

--- 12 Integer to Roman/12integer-to-roman.py
+++ 12 Integer to Roman/12integer-to-roman.py
@@ -1,18 +1,17 @@
 class Solution:
     def intToRoman(self, num: int) -> str:
-        res = [[1, 'I'], [4, 'IV'], [5, 'V'], [9, 'IX'], [10, 'X'], 
-        [40, 'XL'], [50, 'L'], [90, 'XC'], [100, 'C'], 
-        [400, 'CD'], [500, 'D'], [900, 'CM'], [1000, 'M']]
-        
-        new_str = []
-        rem = 0
-        for i in range(len(res) - 1, -1, -1):
-            # print(num, rem)
-            if num >= res[i][0]:
-                rem = num % res[i][0]
-                num //= res[i][0]
-                new_str += (res[i][1] * num)
-                num = rem
-    
-        return ''.join(new_str)
+        arr = [(1, "I"), (4, "IV"), (5, "V"), (9, "IX"), (10, "X"), (40, "XL"), (50, "L"), (90, "XC"), (100, "C"), (400, "CD"), (500, "D"), (900, "CM"), (1000, "M")]
 
+        p1 = len(arr) - 1
+        res = ""
+
+        while p1 >= 0:
+            if num >= arr[p1][0]:
+                count = num // arr[p1][0]
+                num = num % arr[p1][0]
+                res += arr[p1][1] * count
+                p1 -= 1
+            else:
+                p1 -= 1
+        
+        return res
