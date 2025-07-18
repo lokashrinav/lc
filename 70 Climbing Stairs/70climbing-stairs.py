8bfb3618006ca761cd29569a9dcd276bf1801151
Shrinav Loka <lokashrinav@gmail.com>
2025-07-18 22:52:43 +0000

    Code challenge submission for question: 70 Climbing Stairs

--- 70 Climbing Stairs/70climbing-stairs.py
+++ 70 Climbing Stairs/70climbing-stairs.py
@@ -1,9 +1,13 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-        
-        prev1, prev2 = 1, 2
 
-        for i in range(2, n+1):
-            prev1, prev2 = prev2, prev1 + prev2
+        # dp[i] = dp[i - 1] + dp[i - 2]
 
-        return prev1
+        prev1, prev2 = 1, 1
+
+        for i in range(2, n + 1):
+            val = prev1 + prev2
+            prev1 = prev2
+            prev2 = val
+                
+        return prev2
