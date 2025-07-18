eadf24d912b1d731dfd68cf6acb0474ea9732a33
Shrinav Loka <lokashrinav@gmail.com>
2025-07-18 22:44:56 +0000

    Code challenge submission for question: 412 Fizz Buzz

--- 412 Fizz Buzz/412fizz-buzz.py
+++ 412 Fizz Buzz/412fizz-buzz.py
@@ -1,6 +1,7 @@
 class Solution:
     def fizzBuzz(self, n: int) -> List[str]:
         res = []
+        
         for i in range(1, n + 1):
             if i % 3 == 0 and i % 5 == 0:
                 res.append("FizzBuzz")
@@ -10,6 +11,5 @@ class Solution:
                 res.append("Buzz")
             else:
                 res.append(str(i))
-        return res 
-
         
+        return res
