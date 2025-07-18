93cf7f5fb695c054fdceb919a59a6a5c73632fed
Shrinav Loka <lokashrinav@gmail.com>
2025-07-18 23:03:44 +0000

    Code challenge submission for question: 121 Best Time to Buy and Sell Stock

--- 121 Best Time to Buy and Sell Stock/121best-time-to-buy-and-sell-stock.py
+++ 121 Best Time to Buy and Sell Stock/121best-time-to-buy-and-sell-stock.py
@@ -1,13 +1,16 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        minSoFar = float('inf')
-
         maxProf = 0
-        for i in range(len(prices)):
-            if prices[i] < minSoFar:
-                minSoFar = prices[i]
-            else:
-                maxProf = max(maxProf, prices[i] - minSoFar)
+        '''
+        [7, 1, 5, 3, 6, 4]
+        '''
+        curr = prices[0]
+        for i in range(1, len(prices)):
+            if prices[i] < curr:
+                curr = prices[i]
+            maxProf = max(prices[i] - curr, maxProf)
         
         return maxProf
+
+
         
