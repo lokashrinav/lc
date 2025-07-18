53edaa6ae4a4c40346660c28b763fcf8633ae9c3
Shrinav Loka <lokashrinav@gmail.com>
2025-07-18 22:33:29 +0000

    Code challenge submission for question: 15 3Sum

--- 15 3Sum/153sum.py
+++ 15 3Sum/153sum.py
@@ -1,29 +1,25 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        
         nums.sort()
         res = []
-        i = 0
-        while i < len(nums):
-            if i > 0 and nums[i] == nums[i - 1]:
-                i += 1
+
+        for i in range(len(nums)):
+            if i >= 1 and nums[i] == nums[i - 1]:
                 continue
-
-            l, r = i + 1, len(nums) - 1
-            while l < r:
-                if nums[i] + nums[l] + nums[r] > 0:
-                    r -= 1
-                elif nums[i] + nums[l] + nums[r] < 0:
-                    l += 1
+            p1 = i + 1
+            p2 = len(nums) - 1
+            while p1 < p2:
+                if p2 + 1 < len(nums) and nums[p2] == nums[p2 + 1]:
+                    p2 -= 1
+                elif p1 - 1 > i and nums[p1] == nums[p1 - 1]:
+                    p1 += 1
+                elif nums[p1] + nums[p2] == -nums[i]:
+                    res.append([nums[p1], nums[p2], nums[i]])
+                    p2 -= 1
+                    p1 += 1
+                elif nums[p1] + nums[p2] > -nums[i]:
+                    p2 -= 1
                 else:
-                    res.append([nums[i], nums[l], nums[r]])
-                    l += 1
-                    r -= 1
-                    while l < r and nums[r] == nums[r + 1]:
-                        r -= 1
-
-                    while l < r and nums[l] == nums[l - 1]:
-                        l += 1
-            i += 1
-        # -3 -3 1 1 2
+                    p1 += 1
+        
         return res
