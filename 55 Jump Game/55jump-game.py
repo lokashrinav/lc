f79f3db21aa56dc77ad3ca8b9ba8568730ce40e9
Shrinav Loka <lokashrinav@gmail.com>
2025-07-21 06:35:27 +0000

    Code challenge submission for question: 55 Jump Game

--- 55 Jump Game/55jump-game.py
+++ 55 Jump Game/55jump-game.py
@@ -1,9 +1,20 @@
 class Solution:
     def canJump(self, nums: List[int]) -> bool:
-        goal = len(nums) - 1
-        for i in range(len(nums) - 2, -1, -1):
-            if nums[i] + i >= goal:
-                goal = i
-        return goal == 0
+        n = len(nums)
+        dp = [False] * n
+        dp[-1] = True  # We can always reach the end from the end
+
+        for i in range(n - 2, -1, -1):
+            farthest = min(i + nums[i], n - 1)
+            for j in range(i + 1, farthest + 1):
+                if dp[j]:
+                    dp[i] = True
+
+        return dp[0]
+
+
+                    
 
         
+
+            
