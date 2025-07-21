20f43059fb25258b2ba46f9a953ac5cda3299472
Shrinav Loka <lokashrinav@gmail.com>
2025-07-21 17:51:19 +0000

    Code challenge submission for question: 64 Minimum Path Sum

--- 64 Minimum Path Sum/64minimum-path-sum.py
+++ 64 Minimum Path Sum/64minimum-path-sum.py
@@ -1,15 +1,21 @@
 class Solution:
     def minPathSum(self, grid: List[List[int]]) -> int:
-        dp = [[float('inf') for _ in range(len(grid[0]) + 1)] for p in range(len(grid) + 1)]
 
+        '''
+
+        dp[i][j] = dp[i][j] + min(dp[i + 1][j], dp[i][j + 1])
+
+        '''
+
+        dp = grid
         for i in range(len(grid) - 1, -1, -1):
-            for p in range(len(grid[0]) - 1, -1, -1):
-                if i == len(grid) - 1 and len(grid[0]) - 1 == p:
-                    dp[i][p] = grid[i][p]
-                else:
-                    dp[i][p] = min(dp[i + 1][p], dp[i][p + 1]) + grid[i][p]
-
-        print(dp)
+            for j in range(len(grid[0]) - 1, -1, -1):
+                if i + 1 < len(grid) and j + 1 < len(grid[0]):
+                    dp[i][j] += min(dp[i + 1][j], dp[i][j + 1])
+                elif i + 1 < len(grid):
+                    dp[i][j] += dp[i + 1][j]
+                elif j + 1 < len(grid[0]):
+                    dp[i][j] += dp[i][j + 1]
         
         return dp[0][0]
-            
+        
