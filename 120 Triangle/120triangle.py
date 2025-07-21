7051e2c669cb93c7f956cc65bf71b041d599e541
Shrinav Loka <lokashrinav@gmail.com>
2025-07-21 17:58:14 +0000

    Code challenge submission for question: 120 Triangle

--- 120 Triangle/120triangle.py
+++ 120 Triangle/120triangle.py
@@ -1,16 +1,18 @@
 class Solution:
     def minimumTotal(self, triangle: List[List[int]]) -> int:
-        dp = [[0] * (len(triangle) + 1) for _ in range(len(triangle) + 1)]
+        '''
 
-        for i in range(len(triangle) - 1, -1, -1):
-            for p in range(len(triangle[i]) - 1, -1, -1):
-                dp[i][p] = triangle[i][p] + min(dp[i + 1][p], dp[i + 1][p + 1])
-        print(dp)
-        return dp[0][0]
+        triangle[i][p] += min(triangle[i - 1][p - 1], triangle[i - 1][p])
 
+        '''
 
-
-
+        for i in range(1, len(triangle)):
+            triangle[i][0] += triangle[i - 1][0]
+            triangle[i][-1] += triangle[i - 1][-1]
+            for p in range(1, len(triangle[i]) - 1):
+                triangle[i][p] += min(triangle[i - 1][p - 1], triangle[i - 1][p])
+            #print(triangle)
         
+        return min(triangle[-1])
 
         
