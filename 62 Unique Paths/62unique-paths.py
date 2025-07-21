c2e35fef36b2627121ca6c9b58432e17a8764a13
Shrinav Loka <lokashrinav@gmail.com>
2025-07-21 06:40:23 +0000

    Code challenge submission for question: 62 Unique Paths

--- 62 Unique Paths/62unique-paths.py
+++ 62 Unique Paths/62unique-paths.py
@@ -1,4 +1,19 @@
-import math 
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
-        return math.comb(m + n - 2, m - 1)
+
+        dp = [[0] * (n + 1) for i in range(m + 1)]
+
+        '''
+
+        dp[i][j] = dp[i][j - 1] + dp[i - 1][j]
+
+        '''
+
+        dp[1][1] = 1
+
+        for i in range(m):
+            for j in range(n):
+                dp[i + 1][j + 1] += dp[i][j + 1] + dp[i + 1][j]
+        
+        return dp[-1][-1]
+        
