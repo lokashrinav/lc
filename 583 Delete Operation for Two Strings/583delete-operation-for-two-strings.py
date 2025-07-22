743aba805123a2a3a1ee8f7d1f2f6afeb51bf5f0
Shrinav Loka <lokashrinav@gmail.com>
2025-07-22 05:12:20 +0000

    Code challenge submission for question: 583 Delete Operation for Two Strings

--- 583 Delete Operation for Two Strings/583delete-operation-for-two-strings.py
+++ 583 Delete Operation for Two Strings/583delete-operation-for-two-strings.py
@@ -1,15 +1,20 @@
 class Solution:
     def minDistance(self, word1: str, word2: str) -> int:
+        '''
+
+        dp[i][j] = dp[i - 1][j - 1] if word1[i] == word2[i] else 1 + max(dp[i - 1][j], dp[i][j - 1])
+
+        '''
         
         dp = [[0] * (len(word2) + 1) for i in range(len(word1) + 1)]
 
-        for y in range(len(word1)):
-            for x in range(len(word2)):
-                if word1[y] == word2[x]:
-                    dp[y + 1][x + 1] += dp[y][x] + 1
+        for i in range(1, len(word1) + 1):
+            for p in range(1, len(word2) + 1):
+                if word1[i - 1] == word2[p - 1]:
+                    dp[i][p] = 1 + dp[i - 1][p - 1]
                 else:
-                    dp[y + 1][x + 1] = max(dp[y][x+1], dp[y+1][x])  
-        
-        print(max(len(word1), len(word2)))
-        print(dp[len(word1)][len(word2)])
-        return len(word1) + len(word2) - (dp[len(word1)][len(word2)] * 2)
+                    dp[i][p] = max(dp[i - 1][p], dp[i][p - 1])
+
+        print(dp)
+
+        return len(word1) + len(word2) - dp[-1][-1] * 2
