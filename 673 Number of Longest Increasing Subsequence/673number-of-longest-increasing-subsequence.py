1551924038e455bd4faaf1af5131681e21ceb79b
Shrinav Loka <lokashrinav@gmail.com>
2025-07-24 03:37:51 +0000

    Code challenge submission for question: 673 Number of Longest Increasing Subsequence

--- 673 Number of Longest Increasing Subsequence/673number-of-longest-increasing-subsequence.py
+++ 673 Number of Longest Increasing Subsequence/673number-of-longest-increasing-subsequence.py
@@ -1,36 +1,37 @@
 class Solution:
     def findNumberOfLIS(self, nums: List[int]) -> int:
-        dp = [0] * len(nums)
-        dp[0] = 1
-        length = 1
-        for i in range(1, len(nums)):
-            for p in range(i):
-                if nums[i] > nums[p]:
-                    dp[i] += dp[p]
+        
+        '''
+            dp[0] = 1
+            dp[1] = 1
+            dp[2] = 
+        '''
 
         dp = [1] * len(nums)
-        dp[0] = 1
         dp2 = [1] * len(nums)
-        dp2[0] = 1
-        length = 1
 
-        for i in range(1, len(nums)):
-            for p in range(i):
+        for i in range(len(nums)):
+            for p in range(i - 1, -1, -1):
                 if nums[i] > nums[p]:
-                    if dp[p] + 1 > dp[i]:
-                        dp[i] = dp[p] + 1
-                        dp2[i] = dp2[p]
-                    elif dp[i] == dp[p] + 1:
-                        dp2[i] += dp2[p]
+                    dp[i] = max(dp[i], 1 + dp[p])
+            for p in range(i - 1, -1, -1):
+                if nums[p] < nums[i] and dp[i] == dp[p] + 1:
+                    dp2[i] += dp2[p]
+            if dp2[i] != 1:
+                dp2[i] -= 1
+        
+        print(dp, dp2)
 
-        maxNum = max(dp)
+        final = max(dp)
 
-        smth = 0
-        for i in range(len(dp)):
-            if dp[i] == maxNum:
-                smth += dp2[i]
+        answer = sum(c for l, c in zip(dp, dp2) if l == final)
 
-        return smth
+        return answer
+
+
+        
+            
+
 
         
             
