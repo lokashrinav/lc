5e6509260e2a72a29b71092ca2f93d09d30b5f7d
Shrinav Loka <lokashrinav@gmail.com>
2025-07-26 07:58:42 +0000

    Code challenge submission for question: 207 Course Schedule

--- 207 Course Schedule/207course-schedule.py
+++ 207 Course Schedule/207course-schedule.py
@@ -1,59 +1,51 @@
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        def dfs(node, hmap):
-            if node in visited:
-                return False
-            if node not in hmap.keys() or hmap[node] == []:
-                return True
-            visited.add(node)
-            for i in range(len(hmap[node])):
-                if dfs(hmap[node][i], hmap) == False:
-                    return False
-            visited.remove(node)
-            hmap[node] = []
-            return True
+        paths = defaultdict(list)
+        for y, x in prerequisites:
+            paths[x].append(y)
         
-        hmap = {}
         visited = set()
+        curr = set()
 
-        for x, y in prerequisites:
-            if x not in hmap.keys():
-                hmap[x] = []
-            hmap[x].append(y)
+        '''
+        0 -> 1
+        0 -> 2
+        1 -> 3
+        2 -> 3
 
-        for i in hmap.keys():
-            if dfs(i, hmap) == False:
+        '''
+
+        def dfs(num):
+            if num in curr:
+                return True
+            if num in visited:
                 return False
-            
 
+            curr.add(num)
+            visited.add(num)
+
+            for elem in paths[num]:
+                if dfs(elem):
+                    return True
+            
+            curr.remove(num)
+            
+            return False  
+
+        '''
+
+        4 -> 1
+        4 -> 2
+        3 -> 1
+        3 -> 2
+
+        '''      
+
+        for i in range(numCourses):
+            curr = set()
+            if dfs(i):
+                return False
+        
         return True
-        
-            
-            
-
-
-
-                
-        '''while condition:
-            slow = prerequisites[slow]
-
-            if slow == fast:
-                return False'''
-        
-        return True
-        
-        # [1, 0], [1, 2], [2, 0], [3, 2], [3, 1], [5, 7]
-
-        # 0 -> 1
-        # 0 -> 2
-        # 1 -> 2
-        # 1 -> 3
-        # 5 -> 7
-        # 2 -> 3
 
         
-
-
-
-
-
