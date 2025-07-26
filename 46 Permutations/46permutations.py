ed7eebef3a2020532bb742e028085aeb4617d812
Shrinav Loka <lokashrinav@gmail.com>
2025-07-26 07:23:35 +0000

    Code challenge submission for question: 46 Permutations

--- 46 Permutations/46permutations.py
+++ 46 Permutations/46permutations.py
@@ -1,20 +1,23 @@
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
-        final = []
-
-        def helper(arr, space):
-            if len(space) == 0:
-                final.append(arr.copy())
-                return
-
-            for i in space:
-                arr.append(i)
-                x = space.copy()
-                x.remove(i)
-                helper(arr, x)
-                removed = arr.pop()
-                x.add(removed)
-
-        helper([], set(nums))
-        return final
         
+        res = []
+        curr = []
+        arr = [True] * len(nums)
+
+        def dfs(curr):
+            if len(curr) == len(nums):
+                res.append(curr.copy())
+                return 
+            
+            for i in range(len(nums)):
+                if arr[i]:
+                    curr.append(nums[i])
+                    arr[i] = False
+                    dfs(curr)
+                    arr[i] = True
+                    curr.pop()
+        
+        dfs(curr)
+
+        return res
