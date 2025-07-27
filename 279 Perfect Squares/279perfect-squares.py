37f26247279ef4b5ac62ee4018aa7bef458ee527
Shrinav Loka <lokashrinav@gmail.com>
2025-07-27 13:35:57 +0000

    Code challenge submission for question: 279 Perfect Squares

--- 279 Perfect Squares/279perfect-squares.py
+++ 279 Perfect Squares/279perfect-squares.py
@@ -1,26 +1,19 @@
-import math
 class Solution:
     def numSquares(self, n: int) -> int:
 
-        '''
-        [0, 1, 2, 3, 1, 2, 3, 4, 2, 1, ]
-        '''
-
-        dp = [0] * (n + 1)
-
-        coins = [1]
+        dp = [float('inf')] * (n + 1)
+        dp[0] = 0
 
         for i in range(1, n + 1):
-            x = math.sqrt(i)
-            if(x % 1 == 0):
-                coins.append(i)
-            minNum = float('inf')
-            for p in coins:
-                minNum = min(1 + dp[i - p], minNum)
-            dp[i] = minNum
+            p = 1
+            while p * p <= i:
+                dp[i] = min(dp[i], 1 + dp[i - (p * p)])
+                p += 1
+        
         return dp[-1]
 
 
 
+        
 
         
