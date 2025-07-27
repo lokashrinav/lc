bb079490dd606d1aad616def3cc1b210bd69897d
Shrinav Loka <lokashrinav@gmail.com>
2025-07-27 20:08:24 +0000

    Code challenge submission for question: 875 Koko Eating Bananas

--- 875 Koko Eating Bananas/875koko-eating-bananas.py
+++ 875 Koko Eating Bananas/875koko-eating-bananas.py
@@ -1,27 +1,19 @@
 class Solution:
     def minEatingSpeed(self, piles: List[int], h: int) -> int:
-        def calculate(piles, k):
-            count = 0
-            for i in piles:
-                count += math.ceil(i / k)
-            return count
 
-        l = 1
-        r = max(piles)
+        l, r = 1, sum(piles)
+        saved = float('inf')
 
-        minHours = float('inf')
         while l <= r:
             m = (l + r) // 2
-            hours = calculate(piles, m)
-            print(m, hours)
-            if hours <= h:
-                minHours = min(minHours, m)
-                r = m - 1
-            elif hours > h:
+            total = 0
+            for i in range(len(piles)):
+                total += math.ceil(piles[i] / m)
+            if total > h:
                 l = m + 1
-        if minHours == float('inf'):
-            return 0  
-        return minHours
-
-
+            else:
+                saved = min(saved, m)
+                r = m - 1
         
+        return saved
+            
