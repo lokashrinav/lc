f6b9b9c58c71334af6e80172f76fbca13b0f1f2e
Shrinav Loka <lokashrinav@gmail.com>
2025-07-27 16:43:14 +0000

    Code challenge submission for question: 56 Merge Intervals

--- 56 Merge Intervals/56merge-intervals.py
+++ 56 Merge Intervals/56merge-intervals.py
@@ -1,12 +1,18 @@
 class Solution:
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
-        res = []
-        intervals.sort()
-        for i in range(len(intervals) - 1):
-            if intervals[i][1] >= intervals[i + 1][0]:
-                intervals[i + 1] = [min(intervals[i][0], intervals[i + 1][0]), max(intervals[i][1], intervals[i + 1][1])]
-            else:
-                res.append(intervals[i])
-        res.append(intervals[-1])
-        return res
         
+
+        intervals.sort()
+        prev = intervals[0]
+        res = []
+
+        for i in range(1, len(intervals)):
+            if prev[1] >= intervals[i][0]:
+                prev = [prev[0], max(intervals[i][1], prev[1])]
+            else:
+                res.append(prev)
+                prev = intervals[i]
+        
+        res.append(prev)
+
+        return res
