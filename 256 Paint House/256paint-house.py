71ac9ea21ffd92be6245ee7858b3651ccd3c7b63
Shrinav Loka <lokashrinav@gmail.com>
2025-07-27 09:39:48 +0000

    Code challenge submission for question: 256 Paint House

--- 256 Paint House/256paint-house.py
+++ 256 Paint House/256paint-house.py
@@ -1,21 +1,14 @@
 class Solution:
     def minCost(self, costs: List[List[int]]) -> int:
         '''
-        [
-            17, 5
-            2, 5
-            17, 16
-        ]
+        
+        dp[i][0] = max(dp[i - 1][1], dp[i - 1][2])
+
         '''
 
-        dp = [[0] * 3 for i in range(len(costs))]
-
-        for i in range(len(costs)):
-            if i == 0:
-                dp[i] = costs[i]
-            else:
-                dp[i][0] = costs[i][0] + min(dp[i - 1][1], dp[i - 1][2])
-                dp[i][1] = costs[i][1] + min(dp[i - 1][0], dp[i - 1][2])
-                dp[i][2] = costs[i][2] + min(dp[i - 1][0], dp[i - 1][1])
+        dp = [[0] * 3 for _ in range(len(costs) + 1)]
+        for i in range(1, len(costs) + 1):
+            for p in range(3):
+                dp[i][p] = min(dp[i - 1][(p + 1) % 3], dp[i - 1][(p + 2) % 3]) + costs[i - 1][p]
         
         return min(dp[-1])
