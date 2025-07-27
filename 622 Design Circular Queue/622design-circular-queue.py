95003c5e7e2cf8272f61339669d4fd38885e7f4d
Shrinav Loka <lokashrinav@gmail.com>
2025-07-27 13:54:11 +0000

    Code challenge submission for question: 622 Design Circular Queue

--- 622 Design Circular Queue/622design-circular-queue.py
+++ 622 Design Circular Queue/622design-circular-queue.py
@@ -1,63 +1,61 @@
-class ListNode:
+class Node:
     def __init__(self, val):
+        self.next = None
+        self.prev = None
         self.val = val
-        self.next = None  
 
 class MyCircularQueue:
 
     def __init__(self, k: int):
-        self.cap = k
-        self.size = 0
+        self.k = k
         self.head = None
-        self.tail = None       
+        self.tail = None
+        self.cap = 0
 
     def enQueue(self, value: int) -> bool:
-        if self.size + 1 > self.cap:
+        if self.cap == self.k:
             return False
 
-        if not self.head and not self.tail:
-            self.head = ListNode(value)
-            self.tail = self.head
-        else:
-            self.tail.next = ListNode(value)
-            self.tail = self.tail.next
-        self.size += 1
+        self.cap += 1
+        if not self.tail:
+            self.tail = self.head = curr = Node(value)
+            return True
+        
+        curr = Node(value)
+        self.tail.next = curr
+        curr.prev = self.tail
+        self.tail = curr
+
+        return True
+
+    def deQueue(self) -> bool:
+        if self.isEmpty():
+            return False
+
+        if self.head.next:        
+            self.head.next.prev = None
+        
+        if self.head == self.tail:
+            self.tail = None
+            
+        self.head = self.head.next
+        
+        self.cap -= 1
+
         return True
         
 
-    def deQueue(self) -> bool:
-        if self.head:
-            if self.head == self.tail:
-                self.head = None
-                self.tail = None
-            else:
-                self.head = self.head.next
-            self.size -= 1
-            return True
-        return False        
-
     def Front(self) -> int:
-        if not self.head:
-            return -1
-        return self.head.val
+        return self.head.val if not self.isEmpty() else -1
 
     def Rear(self) -> int:
-        if not self.tail:
-            return -1
-        return self.tail.val
-        
+        return self.tail.val if not self.isEmpty() else -1
+
     def isEmpty(self) -> bool:
-        if self.head == None:
-            return True
-        else:
-            return False
+        return self.head is None
 
     def isFull(self) -> bool:
-        if self.size == self.cap:
-            return True
-        else:
-            return False
-        
+        return self.cap == self.k
 
 
 # Your MyCircularQueue object will be instantiated and called as such:
