91afa4f2e394083493da2fa837cf9b7069de3f91
Shrinav Loka <lokashrinav@gmail.com>
2025-07-27 14:15:56 +0000

    Code challenge submission for question: 399 Evaluate Division

--- 399 Evaluate Division/399evaluate-division.py
+++ 399 Evaluate Division/399evaluate-division.py
@@ -1,37 +1,39 @@
 class Solution:
     def calcEquation(self, equations: List[List[str]], values: List[float], queries: List[List[str]]) -> List[float]:
-
-        def dfs(curr, end, prod):
-            if curr == end:
-                return prod
-            
-            minNum = -1
-            for num in graph[curr]:
-                if num not in visited:
-                    visited.add(num)
-                    minNum = max(minNum, dfs(num, end, prod * graph[curr][num]))
-                    if minNum != -1:
-                        return minNum
-            
-            return minNum
-
-
-        graph = defaultdict(dict)
-
-        for i, (num, dem) in enumerate(equations):
-            graph[num][dem] = values[i]
-            graph[dem][num] = 1 / values[i]
         
+        paths = defaultdict(list)
+        ind = 0
+
+        for y, x in equations:
+            paths[y].append((x, False, values[ind]))
+            paths[x].append((y, True, values[ind]))
+            ind += 1
+        
+        def dfs(y, x):
+            if y == x:
+                return 1
+            if y in visited:
+                return -1
+
+            visited.add(y)
+
+            for elem, idk, fun in paths[y]:
+                calc = dfs(elem, x) 
+                if calc != -1:
+                    if not idk:
+                        return fun * calc
+                    else:
+                        return calc / fun
+            
+            return -1
+
         res = []
-        for num, dem in queries:
-            if num not in graph or dem not in graph:
-                res.append(float(-1))
-            elif num == dem:
-                res.append(float(1))
-            else:
-                visited = set()
-                res.append(dfs(num, dem, 1))
-        
-        return res
+        for y, x in queries:
+            visited = set()
+            if y not in paths or x not in paths:
+                res.append(-1)
+                continue
 
-        
+            res.append(dfs(y, x))
+    
+        return res
