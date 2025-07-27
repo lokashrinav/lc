560e2895614bd1b9db2173d6e0888e8f441b1d55
Shrinav Loka <lokashrinav@gmail.com>
2025-07-27 17:07:11 +0000

    Code challenge submission for question: 221 Maximal Square

--- 221 Maximal Square/221maximal-square.py
+++ 221 Maximal Square/221maximal-square.py
@@ -1,41 +1,29 @@
 class Solution:
     def maximalSquare(self, matrix: List[List[str]]) -> int:
+        '''
 
-        if len(matrix) == 1:
-            is_one = False
-            for i in range(len(matrix[0])):
-                if matrix[0][i] == "1":
-                    is_one = True
-            if is_one:
-                return 1
-            return 0
+        dp[i][j] = 1 + min(dp[i - 1][j], dp[i][j - 1])
+
+        ["1","0","1","0"],
+        ["1","0","1","1"],
+        ["1","0","1","1"],
+        ["1","1","1","1"]
+
+        '''
+
+        dp = [[0] * (len(matrix[0]) + 1) for i in range(len(matrix) + 1)]
+
+        for y in range(1, len(matrix) + 1):
+            for x in range(1, len(matrix[0]) + 1):
+                if matrix[y - 1][x - 1] == "1":
+                    dp[y][x] = min(dp[y - 1][x], dp[y][x - 1], dp[y - 1][x - 1]) + 1
         
-        if len(matrix[0]) == 1:
-            is_one = False
-            for i in range(len(matrix)):
-                if matrix[i][0] == "1":
-                    is_one = True
-            if is_one:
-                return 1
-            return 0
-
-        dp = [[0] * len(matrix[0]) for _ in range(len(matrix))]
-
-        for i in range(len(matrix[0])):
-            dp[-1][i] = int(matrix[-1][i])
-        
-        for i in range(len(matrix)):
-            dp[i][-1] = int(matrix[i][-1])
-
-        for i in range(len(matrix) - 2, -1, -1):
-            for p in range(len(matrix[0]) - 2, -1, -1):
-                if matrix[i][p] == "0":
-                    dp[i][p] = 0
-                else:
-                    dp[i][p] = 1 + min(int(dp[i + 1][p + 1]), int(dp[i + 1][p]), int(dp[i][p + 1]))
+        maxNum = 0
+        for y in range(len(dp)):
+            for x in range(len(dp[0])):
+                maxNum = max(maxNum, dp[y][x] * dp[y][x])
 
         print(dp)
-
-        return max(max(x) for x in dp) ** 2
-
+        
+        return maxNum
         
