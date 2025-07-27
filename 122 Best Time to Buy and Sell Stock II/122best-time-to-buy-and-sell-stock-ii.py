7aa9edd702bf9d027732aa4effd9b6eb88e5bdfd
Shrinav Loka <lokashrinav@gmail.com>
2025-07-27 16:37:44 +0000

    Code challenge submission for question: 122 Best Time to Buy and Sell Stock II

--- 122 Best Time to Buy and Sell Stock II/122best-time-to-buy-and-sell-stock-ii.py
+++ 122 Best Time to Buy and Sell Stock II/122best-time-to-buy-and-sell-stock-ii.py
@@ -1,16 +1,28 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        dp = [[0, 0] for _ in range(len(prices) + 1)]
+        '''
+        dfs(ind, False) = max(dfs(ind - 1, True) - prices[ind], dfs(ind + 1, False))
+        dfs(ind, True) = max(prices[ind] + dfs(ind - 1, True), dfs(ind - 1, False))
+
+        7, 1, 5, 3, 6, 4
+
+
+        1, 5
+
+        dp[1][0] = 5
+        dp[1][1] = 0
+
+        dp[0][0] = 
+        dp[0][1] = 
+
+        '''
+
+        dp = [[0, 0] for i in range(len(prices) + 1)]
 
         for i in range(len(prices) - 1, -1, -1):
-            buy = max(-prices[i] + dp[i + 1][0], dp[i + 1][1])
-            sell = max(prices[i] + dp[i + 1][1], dp[i + 1][0])
-            dp[i][1] = buy
-            dp[i][0] = sell
-                    
+            dp[i][0] = max(dp[i + 1][0], dp[i + 1][1] + prices[i])  # Sell: add price
+            dp[i][1] = max(dp[i + 1][1], dp[i + 1][0] - prices[i])  # Buy: subtract price
+        
+        print(dp)
+
         return dp[0][1]
-
-
-
-        
-        
