ebb295a3f1052b2e4e8eb6caf120277ec7794ea1
Shrinav Loka <lokashrinav@gmail.com>
2025-07-27 11:52:45 +0000

    Code challenge submission for question: 48 Rotate Image

--- 48 Rotate Image/48rotate-image.py
+++ 48 Rotate Image/48rotate-image.py
@@ -2,15 +2,16 @@ class Solution:
     def rotate(self, matrix: List[List[int]]) -> None:
         """
         Do not return anything, modify matrix in-place instead.
+        0, 1
+        1, 2
         """
+        
         for y in range(len(matrix)):
-            for x in range(y, len(matrix[0])):
-                tmp = matrix[y][x]
-                matrix[y][x] = matrix[x][y]
-                matrix[x][y] = tmp
-                
-        for row in matrix:
-            row.reverse()
-
+            for x in range(y + 1, len(matrix[0])):
+                matrix[y][x], matrix[x][y] = matrix[x][y], matrix[y][x]
         
+        for y in range(len(matrix)):
+            for x in range(len(matrix[0]) // 2):
+                matrix[y][len(matrix[0]) - x - 1], matrix[y][x] = matrix[y][x], matrix[y][len(matrix[0]) - x - 1]
         
+        return matrix
