4ae4b14346169ae785d6f41182db9ad71b8e5bdf
Shrinav Loka <lokashrinav@gmail.com>
2025-07-28 19:16:22 +0000

    Code challenge submission for question: 329 Longest Increasing Path in a Matrix

--- 329 Longest Increasing Path in a Matrix/329longest-increasing-path-in-a-matrix.py
+++ 329 Longest Increasing Path in a Matrix/329longest-increasing-path-in-a-matrix.py
@@ -1,47 +1,34 @@
 class Solution:
     def longestIncreasingPath(self, matrix: List[List[int]]) -> int:
+        '''
+        (2, 1) = 1 -> [2, 6]
+        (2, 2) = 1 -> [8]
+        (2, 0) = 2 -> [6]
+        (1, 0) = 6 -> [9]
+        '''
 
-        dp = [[None for i in range(len(matrix[0]))] for p in range(len(matrix))]
+        dirs = [[0, 1], [1, 0], [-1, 0], [0, -1]]
+        def dfs(out):
+            y, x = out
+            if out in dp:
+                return dp[out]
+            
+            curr = 0
+            for ny, nx in dirs:
+                dy, dx = y + ny, x + nx
+                if len(matrix) > dy >= 0 and len(matrix[0]) > dx >= 0 and matrix[dy][dx] > matrix[y][x]:
+                    curr = max(curr, dfs((dy, dx)))
 
-        def dfs(index, prevNum, prevIndex):
-            y, x = index
-            if not (y >= 0 and x >= 0 and y < len(matrix) and x < len(matrix[0])):
-                return 0
-
-            currNum = matrix[y][x]
-
-            if prevNum >= currNum:
-                return 0
-
-            if dp[y][x]:
-                return dp[y][x]
-
-            total = float('-inf')
-            if prevIndex != (y + 1, x):
-                total = max(dfs((y + 1, x), currNum, index) + 1, total)
-            if prevIndex != (y - 1, x):
-                total = max(dfs((y - 1, x), currNum, index) + 1, total)
-            if prevIndex != (y, x + 1):
-                total = max(dfs((y, x + 1), currNum, index) + 1, total)
-            if prevIndex != (y, x - 1):
-                total = max(dfs((y, x - 1), currNum, index) + 1, total)
-
-            dp[y][x] = total
-
-            return dp[y][x]
-
-        maxNum = float('-inf')
-
-        for i in range(len(matrix)):
-            for p in range(len(matrix[0])):
-                if not dp[i][p]:
-                    dp[i][p] = dfs((i, p), -1, (-1, -1))
-
-        print(dp)
-
-        for i in range(len(matrix)):
-            for p in range(len(matrix[0])):
-                maxNum = max(dp[i][p], maxNum)
-
+            dp[(y, x)] = curr + 1
+            return dp[(y, x)]
+        
+        maxNum = 0
+        dp = {}
+        for y in range(len(matrix)):
+            for x in range(len(matrix[0])):
+                maxNum = max(maxNum, dfs((y, x)))
+        
         return maxNum
+                
 
+        
