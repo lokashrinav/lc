f0bee247e1fdd3045ec53954474b8c84eea90083
Shrinav Loka <lokashrinav@gmail.com>
2025-07-28 02:17:23 +0000

    Code challenge submission for question: 239 Sliding Window Maximum

--- 239 Sliding Window Maximum/239sliding-window-maximum.py
+++ 239 Sliding Window Maximum/239sliding-window-maximum.py
@@ -1,33 +1,24 @@
 class Solution:
     def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
         
-        res = []
         queue = deque()
-        for i in range(k):
-            if not queue:
-                queue.append(nums[i])
-            elif nums[i] <= queue[-1]:
-                queue.append(nums[i])
-            else:
-                while queue and nums[i] > queue[-1]:
-                    queue.pop()
-                queue.append(nums[i])
+        res = []
 
-        res.append(queue[0])
+        for i in range(k):
+            while queue and nums[i] >= nums[queue[-1]]:
+                out = queue.pop()
+            queue.append(i)
+    
+        res.append(nums[queue[0]])
+
+        print(queue)
         
         for i in range(k, len(nums)):
-            if nums[i - k] == queue[0]:
+            if i - k == queue[0]:
                 queue.popleft()
-            if not queue:
-                queue.append(nums[i])
-            elif nums[i] <= queue[-1]:
-                queue.append(nums[i])
-            else:
-                while queue and nums[i] > queue[-1]:
-                    queue.pop()
-                queue.append(nums[i])
-            res.append(queue[0])
+            while queue and nums[i] >= nums[queue[-1]]:
+                out = queue.pop()
+            queue.append(i)
+            res.append(nums[queue[0]])
         
         return res
-        
-        
