234b49bb147973ed3d5d7fe566fc3f0ba0727f6c
Shrinav Loka <lokashrinav@gmail.com>
2025-07-29 12:09:10 +0000

    Code challenge submission for question: 4 Median of Two Sorted Arrays

--- 4 Median of Two Sorted Arrays/4median-of-two-sorted-arrays.py
+++ 4 Median of Two Sorted Arrays/4median-of-two-sorted-arrays.py
@@ -1,33 +1,36 @@
-from typing import List
-
-class Solution:
+2702. Minimum Operations to Make Numbers Non-positiveclass Solution:
     def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
-        # Ensure nums1 is the smaller array
-        if len(nums2) < len(nums1):
-            return self.findMedianSortedArrays(nums2, nums1)
-        
-        A, B = nums1, nums2
-        total = len(A) + len(B)
-        half = total // 2
-        l, r = 0, len(A) - 1
-        
-        while True:
-            i = (l + r) // 2  # A
-            j = half - i - 2  # B
 
-            Aleft = A[i] if i >= 0 else float("-infinity")
-            Aright = A[i + 1] if (i + 1) < len(A) else float("infinity")
-            Bleft = B[j] if j >= 0 else float("-infinity")
-            Bright = B[j + 1] if (j + 1) < len(B) else float("infinity")
+        if len(nums1) < len(nums2):
+            nums2, nums1 = nums1, nums2
 
-            # partition is correct
-            if Aleft <= Bright and Bleft <= Aright:
-                # odd
-                if total % 2:
-                    return min(Aright, Bright)
-                # even
-                return (max(Aleft, Bleft) + min(Aright, Bright)) / 2
-            elif Aleft > Bright:
-                r = i - 1
-            else:
-                l = i + 1
+        l, r = 0, len(nums1)
+
+        '''
+
+        2, 3, 7
+        4, 5
+
+        i + 1
+
+        i = 0 -> j - 1
+        i = 1 -> 
+
+        '''
+
+        while l <= r:
+            m = (l + r) // 2
+            j = (len(nums2) + len(nums1) + 1) // 2 - i
+            if nums1 and nums2 and nums1[i] > nums2[j + 1]:
+                r = m - 1
+            elif nums1 and nums2 and nums2[j] > nums1[i + 1]:
+                l = m + 1
+            elif nums1 and nums2 and nums1[i + 1] >= nums2[j] and nums2[j + 1] >= nums1[i]:
+                med = 
+                return med
+
+
+
+
+
+
