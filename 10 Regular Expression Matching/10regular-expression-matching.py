7b025ad5bfc322b2a1342323334fbb3ae2bf233c
Shrinav Loka <lokashrinav@gmail.com>
2025-07-29 06:39:30 +0000

    Code challenge submission for question: 10 Regular Expression Matching

--- 10 Regular Expression Matching/10regular-expression-matching.py
+++ 10 Regular Expression Matching/10regular-expression-matching.py
@@ -1,28 +1,28 @@
 class Solution:
     def isMatch(self, s: str, p: str) -> bool:
-        cache = {}
         
+        @lru_cache(maxsize=None)
         def dfs(i, j):
-            # print(i, j)
-            if (i, j) in cache:
-                return cache[(i, j)]
-            if i == len(s) and j == len(p):
-                return True
+            print(i, j)
+            if i == len(s):
+                if j == len(p):
+                    return True
+                if j + 1 < len(p) and p[j + 1] == "*":
+                    return dfs(i, j + 2)
+                return False
             if j >= len(p):
                 return False
-            
-            match = i < len(s) and (s[i] == p[j] or p[j] == ".")
-            if j + 1 < len(p) and p[j + 1] == "*":
-                cache[(i, j)] = dfs(i, j + 2) or (match and dfs(i + 1, j))
-                return cache[(i, j)]
-            
-            if match:
-                cache[(i, j)] = dfs(i + 1, j + 1)
-                return cache[(i, j)]
-            
-            cache[(i, j)] = False
-            return cache[(i, j)]
+            if j + 1 < len(p) and p[j:j + 2] == ".*":
+                return dfs(i + 1, j) or dfs(i, j + 2)
+            if s[i] != p[j] and j + 1 < len(p) and p[j + 1] == "*":
+                return dfs(i, j + 2)
+            if s[i] == p[j] and j + 1 < len(p) and p[j + 1] == "*":
+                if i == 4 and j == 2:
+                    print("HELLO")
+                return dfs(i, j + 2) or dfs(i + 1, j)
+            if s[i] == p[j] or p[j] == ".":
+                return dfs(i + 1, j + 1)
 
-        return dfs(0, 0)  
+            return False
 
-        
+        return dfs(0, 0)
